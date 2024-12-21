cbd060902dbd068683aa7b3c09c1f4a4fd2c1ebc
Dam Tu <damvantu2004@gmail.com>
2024-12-21 14:34:02 +0700

    thêm vào binding

--- lib/binding.dart
+++ lib/binding.dart
@@ -1,5 +1,8 @@
 
 import 'package:get/get.dart';
+import 'screen/chatbot/controller/chatbot_controller.dart';
+import 'screen/auth/login/controller/login_controller.dart';
+import 'screen/auth/signup/controller/signup_controller.dart';
 
 import 'screen/splash/controller/splash_controller.dart';
 
@@ -12,8 +15,11 @@ class MyBinding extends Bindings {
 
     Get.lazyPut<BottomBarController>(() => BottomBarController(), fenix: true);
     Get.lazyPut<HomeController>(() => HomeController(), fenix: true);
+    Get.lazyPut<LoginController>(() => LoginController(), fenix: true);
+    Get.lazyPut<SignUpController>(() => SignUpController(), fenix: true);
 
     Get.lazyPut<SplashController>(() => SplashController(), fenix: true);
+    Get.lazyPut<ChatBotController>(() => ChatBotController(), fenix: true);
 
 
 
