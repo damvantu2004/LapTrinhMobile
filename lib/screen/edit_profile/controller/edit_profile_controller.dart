385f5f0561780ab0290e3b7626fd44f700808233
Dam Tu <damvantu2004@gmail.com>
2024-12-25 19:04:10 +0700

    sửa xung đột

--- lib/screen/edit_profile/controller/edit_profile_controller.dart
+++ lib/screen/edit_profile/controller/edit_profile_controller.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:furniture_app/data/repository/user_repository.dart';
 import 'package:furniture_app/data/values/strings.dart';
-import 'package:furniture_app/screen/profile/view/profile_page.dart';
+import 'package:furniture_app/screen/profile/view/profile_view.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
